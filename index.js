02b2ee47aac112cc74b00c6ae130697a5e278892
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-05-24 11:34:20 +0100

    Change textures and add about us

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ gsap.to(".second-slide:nth-child(1)", {
   // Film Gallery Images
 const gridImages = document.querySelectorAll(".gallery-grid-item img")
 
-
-gsap.to(".grid-image", {
+if (window.matchMedia("(min-width:700px)").matches) {
+  gsap.to(".grid-image", {
     scrollTrigger: {
       trigger: ".grid-image",
       start: "top bottom", // when the top of the trigger hits the top of the viewport
-      end: "+=700", // end after scrolling 500px beyond the start
+      end: "+=900", // end after scrolling 500px beyond the start
       scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
       stagger:true,
     },
@@ -43,7 +43,7 @@ gsap.to(".grid-image", {
     scrollTrigger: {
       trigger: ".grid-image-two",
       start: "top bottom", // when the top of the trigger hits the top of the viewport
-      end: "+=700", // end after scrolling 500px beyond the start
+      end: "+=900", // end after scrolling 500px beyond the start
       scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
       stagger:true,
     },
@@ -54,14 +54,56 @@ gsap.to(".grid-image", {
     scrollTrigger: {
       trigger: ".grid-image-three",
       start: "top bottom", // when the top of the trigger hits the top of the viewport
-      end: "+=700", // end after scrolling 500px beyond the start
+      end: "+=900", // end after scrolling 500px beyond the start
       scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
       stagger:true,
     },
     y:0,
     opacity:1,
   });
+}
+if (window.matchMedia("(max-width:699px)").matches) {
+  gsap.to(".grid-image", {
+    scrollTrigger: {
+      trigger: ".grid-image",
+      start: "top bottom", // when the top of the trigger hits the top of the viewport
+      end: "+=400", // end after scrolling 500px beyond the start
+      scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+      stagger:true,
+    },
+    y:0,
+    opacity:1,
+  });
+  gsap.to(".grid-image-two", {
+    scrollTrigger: {
+      trigger: ".grid-image-two",
+      start: "top bottom", // when the top of the trigger hits the top of the viewport
+      end: "+=400", // end after scrolling 500px beyond the start
+      scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+      stagger:true,
+    },
+    y:0,
+    opacity:1,
+  });
+  gsap.to(".grid-image-three", {
+    scrollTrigger: {
+      trigger: ".grid-image-three",
+      start: "top bottom", // when the top of the trigger hits the top of the viewport
+      end: "+=400", // end after scrolling 500px beyond the start
+      scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+      stagger:true,
+    },
+    y:0,
+    opacity:1,
+  });
+}
 
+
+
+
+
+
+  
 // Clicker Slider 
 
 
@@ -70,6 +112,32 @@ const clickerSlides = Array.from(clickerTrack.children)
 
 const iconButtons = document.querySelectorAll(".icon-container img")
 
+const nextArrow = document.querySelector(".arrows img:nth-child(2)")
+const prevArrow = document.querySelector(".arrows img:nth-child(1)")
+
+nextArrow.addEventListener("click", (e) => {
+  const currentClickerSlide = document.querySelector(".current-click")
+
+  if (currentClickerSlide.nextElementSibling != null) {
+    currentClickerSlide.classList.remove("current-click")
+    currentClickerSlide.nextElementSibling.classList.add("current-click")
+  } else {
+    return
+  }
+  
+})
+
+prevArrow.addEventListener("click", (e) => {
+  const currentClickerSlide = document.querySelector(".current-click")
+
+  if (currentClickerSlide.previousElementSibling.classList.contains("arrows")) {
+    return
+  } else {
+    currentClickerSlide.classList.remove("current-click")
+    currentClickerSlide.previousElementSibling.classList.add("current-click")
+  }
+  
+})
 
 
 iconButtons.forEach((icon, index) => {
@@ -79,3 +147,4 @@ iconButtons.forEach((icon, index) => {
     clickerSlides[index].classList.add("current-click")
   })
 })
+
