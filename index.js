a43e5d1adbf06801f0de2b44eff45c268763542b
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-05-30 13:56:53 +0100

    Finished

--- index.js
+++ index.js
@@ -114,6 +114,41 @@ if (window.matchMedia("(max-width:699px)").matches) {
     opacity:1,
   });
 }
+if (window.matchMedia("(max-width:350px)").matches) {
+  gsap.to(".grid-image", {
+    scrollTrigger: {
+      trigger: ".grid-image",
+      start: "top bottom", // when the top of the trigger hits the top of the viewport
+      end: "+=50", // end after scrolling 500px beyond the start
+      scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+      stagger:true,
+    },
+    y:0,
+    opacity:1,
+  });
+  gsap.to(".grid-image-two", {
+    scrollTrigger: {
+      trigger: ".grid-image-two",
+      start: "top bottom", // when the top of the trigger hits the top of the viewport
+      end: "+=50", // end after scrolling 500px beyond the start
+      scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+      stagger:true,
+    },
+    y:0,
+    opacity:1,
+  });
+  gsap.to(".grid-image-three", {
+    scrollTrigger: {
+      trigger: ".grid-image-three",
+      start: "top bottom", // when the top of the trigger hits the top of the viewport
+      end: "+=50", // end after scrolling 500px beyond the start
+      scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+      stagger:true,
+    },
+    y:0,
+    opacity:1,
+  });
+}
 
 
 
